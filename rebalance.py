1e709fb7f91baa5d37e948fdee5cff2fbd4f9530
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2017-09-08 18:07:53 -0400

    Rebalance based on a model portfolio

--- rebalance.py
+++ rebalance.py
@@ -6,8 +6,21 @@ from decimal import *
 class Instrument(namedtuple('Instrument', 'symbol, name')):
     __slots__ = ()
     def __repr__(self): return self.symbol
-
 CASH = Instrument('CASH', 'Cash')
+
+##############
+
+class Action(namedtuple('Action', 'direction, name')):
+    __slots__ = ()
+    def __repr__(self): return self.name
+# when sorting, SELL comes before BUY
+# first sell, then buy
+SELL = Action(-1, 'Sell')
+BUY = Action(1, 'Buy')
+
+##############
+
+Order = namedtuple('Order', 'action, instrument, amount')
 
 ##############
 
@@ -16,12 +29,36 @@ class Portfolio:
     def __init__(self, positions):
         self.total = sum(positions.values())
         self.positions = positions
+        self.allocations = self.__calc_allocations()
 
-    @property
-    def allocations(self):
+    def __calc_allocations(self):
         res = defaultdict(Decimal)
         for instrument, value in self.positions.items():
             res[instrument] += value / self.total * 100
-        return dict(res)
+        return res
+
+    def rebalance(self, model_portfolio):
+        target_allocations = self.__resolve_target_allocations(model_portfolio)
+        orders = []
+        for instrument, target_alloc in target_allocations.items():
+            alloc = self.allocations[instrument]
+            alloc_offset = target_alloc - alloc
+            value_offset = self.total * alloc_offset / 100
+            if instrument != CASH and value_offset != 0:
+                action = (BUY if value_offset > 0 else SELL)
+                orders.append(Order(action, instrument, abs(value_offset)))
+        orders.sort()
+        return orders
+
+    def __resolve_target_allocations(self, model_portfolio):
+        # so that all relevant instruments are covered,
+        # start with the model portfolio allocations,
+        # then add the instruments not present there,
+        # but present in this portfolio, with a ZERO allocation
+        res = dict(model_portfolio.allocations)
+        for instrument in self.positions.keys():
+            if instrument not in res:
+                res[instrument] = Decimal(0)
+        return res
 
 ##############
--- test_rebalance.py
+++ test_rebalance.py
@@ -3,8 +3,11 @@ from rebalance import *
 from decimal import *
 
 AAA = Instrument('AAA', 'AAA Index ETF')
+BBB = Instrument('BBB', 'BBB Index ETF')
+CCC = Instrument('CCC', 'CCC Index ETF')
+DDD = Instrument('DDD', 'DDD Index ETF')
 
-class TestPortfolio(unittest.TestCase):
+class PortfolioBasicTest(unittest.TestCase):
 
     def setUp(self):
         self.portfolio = Portfolio({CASH: Decimal(1200), AAA: Decimal(300)})
@@ -19,6 +22,49 @@ class TestPortfolio(unittest.TestCase):
     def test_portfolio_allocations(self):
         self.assertEqual(
             self.portfolio.allocations, {CASH: Decimal(80), AAA: Decimal(20)})
+
+class RebalanceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.model_portfolio = Portfolio({
+            CASH: Decimal(10),
+            AAA: Decimal(40),
+            BBB: Decimal(50)})
+
+    def test_rebalance_cash(self):
+        portfolio = Portfolio({CASH: Decimal(5000)})
+        orders = portfolio.rebalance(self.model_portfolio)
+        self.assertEqual(orders, [
+            (BUY, AAA, Decimal(2000)),
+            (BUY, BBB, Decimal(2500))])
+
+    def test_rebalance_balanced(self):
+        portfolio = Portfolio({
+            CASH: Decimal(500),
+            AAA: Decimal(2000),
+            BBB: Decimal(2500)})
+        orders = portfolio.rebalance(self.model_portfolio)
+        self.assertEqual(orders, [])
+
+    def test_rebalance_imbalanced(self):
+        portfolio = Portfolio({
+            AAA: Decimal(2000),
+            BBB: Decimal(8000)})
+        orders = portfolio.rebalance(self.model_portfolio)
+        self.assertEqual(orders, [
+            (SELL, BBB, Decimal(3000)),
+            (BUY, AAA, Decimal(2000))])
+
+    def test_rebalance_reinvest(self):
+        portfolio = Portfolio({
+            CCC: Decimal(2000),
+            DDD: Decimal(8000)})
+        orders = portfolio.rebalance(self.model_portfolio)
+        self.assertEqual(orders, [
+            (SELL, CCC, Decimal(2000)),
+            (SELL, DDD, Decimal(8000)),
+            (BUY, AAA, Decimal(4000)),
+            (BUY, BBB, Decimal(5000))])
 
 if __name__ == '__main__':
     unittest.main()
