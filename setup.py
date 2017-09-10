b6dc970b65cb37e8f0de33955c3a76a3b971d13b
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2017-09-09 23:22:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 setup(
     name='rebalance',
+    description='Exchange-Traded Funds (ETFs) portfolio rebalancing',
     version='0.0.1',
     author='Rodrigo Jordão',
     author_email='rodrigo.jordao@gmail.com',
     url='https://github.com/jordao76/rebalance',
-    # on Windows, only numpy==1.12.1 worked
-    # the (current) latest 1.13.1 didn't
-    install_requires=['matplotlib==2.0.2', 'numpy==1.12.1'],
-    py_modules=['rebalance'])
+    license='MIT',
+    install_requires=['matplotlib'],
+    test_suite='test_rebalance',
+    py_modules=['rebalance'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'])
